c380b7a4d16065258ea9badfaef9c4b372beb439
zhfrann <muhzhafranilham13@gmail.com>
2025-03-23 17:44:47 +0700

    menambahkan class SayaTubeVideo

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿class SayaTubeVideo
+{
+    private int id, playCount;
+    private string title;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.id = random.Next(10000, 100000);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID: " + this.id);
+        Console.WriteLine("Title: " + this.title);
+        Console.WriteLine("Play Count: " + this.playCount);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo
+}
+}
